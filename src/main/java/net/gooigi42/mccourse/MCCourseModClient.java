8f048dcf930043678267f33d40070383f6a8a1e8
gooigi42 <56941365+gooigi42@users.noreply.github.com>
2024-05-28 01:30:23 -0400

    Initial commit

---
+++ src/main/java/net/gooigi42/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.gooigi42.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gooigi42/mccourse/MCCourseModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gooigi42.mccourse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MCCourseModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
